3dbcc3dbc0209198023d3264049bf47747cdd834
Tushar <99536281+chhikaratushar@users.noreply.github.com>
2024-09-12 21:29:52 +0530

    All the file for Weather App

---
+++ scripts.js
@@ -0,0 +1,46 @@
+const apiKey = 'f85867597d52c81ecbf1beb199a3448c'; // Replace with your OpenWeatherMap API Key
+
+// Function to get weather by user's current location
+function getWeatherByLocation(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+    .then(response => response.json())
+    .then(data => updateWeatherData(data))
+    .catch(error => console.log(error));
+}
+
+// Function to get weather by city input
+function getWeatherByCity() {
+  const city = document.getElementById('city').value;
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+    .then(response => response.json())
+    .then(data => updateWeatherData(data))
+    .catch(error => console.log(error));
+}
+
+// Function to update the weather data on the page
+function updateWeatherData(data) {
+  const cityName = data.name;
+  const temperature = data.main.temp;
+  const description = data.weather[0].description;
+  const weatherIcon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+  // Update HTML
+  document.getElementById('city-name').innerText = `City: ${cityName}`;
+  document.getElementById('temperature').innerText = `Temperature: ${temperature} °C`;
+  document.getElementById('description').innerText = `Condition: ${description}`;
+  document.getElementById('weather-icon').src = weatherIcon;
+
+}
+
+// Get user location and weather data on page load
+window.onload = function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      getWeatherByLocation(lat, lon);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+};
